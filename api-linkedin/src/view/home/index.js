d63cfbd994a4f8615e0637a9e6e339a9bf390689
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-11-03 12:55:32 -0300

    example

--- api-linkedin/src/view/home/index.js
+++ api-linkedin/src/view/home/index.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home() {
-  return <h1>HI</h1>;
+  const [id, setId] = useState('luiz-felipe-lazzaron-682676181');
+  const [data, setData] = useState('');
+
+  function handleChange(value) {
+    setId(value.target.value);
+  }
+
+  function getData() {
+    var unirest = require('unirest');
+
+    var req = unirest('GET', 'https://rapidapi.p.rapidapi.com/rapidapi/');
+
+    req.query({
+      profileId: id,
+    });
+
+    req.headers({
+      'x-rapidapi-key': '8085288af8msh2c14499e1cabd8ep1157f6jsn9d120e529dc6',
+      'x-rapidapi-host': 'linkedin-public-profiles.p.rapidapi.com',
+      useQueryString: true,
+    });
+
+    req.end(function (res) {
+      if (res.error) throw new Error(res.error);
+
+      console.log(JSON.stringify(res.body));
+      setData(JSON.stringify(res.body));
+    });
+  }
+
+  return (
+    <div className='container p-3'>
+      <form>
+        <div className='form-group'>
+          <label>Informe seu id do LinkedIn</label>
+          <input type='text' className='form-control' onChange={handleChange} />
+          <small className='form-text text-muted'>
+            Um exemplo seria <strong>luiz-felipe-lazzaron-682676181</strong>.
+            Podemos usar regex para o usuário por seu <strong>url</strong>
+          </small>
+        </div>
+        <onClick type='submit' className='btn btn-primary'>
+          Buscar
+        </onClick>
+      </form>
+    </div>
+  );
 }
