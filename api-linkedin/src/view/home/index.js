52781579ca7714d50f91669ba3a0ddb70610d47b
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-11-04 12:12:35 -0300

    mongo started

--- api-linkedin/src/view/home/index.js
+++ api-linkedin/src/view/home/index.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 
 export default function Home() {
   const [id, setId] = useState('luiz-felipe-lazzaron-682676181');
-  const [data, setData] = useState('');
+  const [data, setData] = useState({});
+
+  console.log('id:', id);
 
   function handleChange(value) {
     setId(value.target.value);
   }
 
-  function getData() {
+  async function getData() {
     var unirest = require('unirest');
 
     var req = unirest('GET', 'https://rapidapi.p.rapidapi.com/rapidapi/');
@@ -25,27 +27,83 @@ export default function Home() {
 
     req.end(function (res) {
       if (res.error) throw new Error(res.error);
-
-      console.log(JSON.stringify(res.body));
       setData(JSON.stringify(res.body));
     });
   }
 
   return (
     <div className='container p-3'>
-      <form>
-        <div className='form-group'>
-          <label>Informe seu id do LinkedIn</label>
-          <input type='text' className='form-control' onChange={handleChange} />
-          <small className='form-text text-muted'>
-            Um exemplo seria <strong>luiz-felipe-lazzaron-682676181</strong>.
-            Podemos usar regex para o usuário por seu <strong>url</strong>
-          </small>
+      <div className='row'>
+        <div className='col'>
+          <p className='text-center'>Registration</p>
+          <form>
+            <div className='form-group'>
+              <label>E-mail</label>
+              <input
+                type='email'
+                className='form-control'
+                onChange={handleChange}
+              />
+
+              <label>Senha</label>
+              <input
+                type='password'
+                className='form-control'
+                onChange={handleChange}
+              />
+
+              <label>Confirme a Senha</label>
+              <input
+                type='password'
+                className='form-control'
+                onChange={handleChange}
+              />
+              <label>LinkedIn Id</label>
+              <input
+                type='text'
+                className='form-control'
+                onChange={handleChange}
+              />
+              <small className='form-text text-muted'>
+                Um exemplo seria <strong>luiz-felipe-lazzaron-682676181</strong>
+                . Podemos usar regex para o usuário por seu <strong>url</strong>
+              </small>
+            </div>
+            <button
+              // onClick={getData()}
+              className='btn btn-primary'
+            >
+              Register
+            </button>
+          </form>
         </div>
-        <onClick type='submit' className='btn btn-primary'>
-          Buscar
-        </onClick>
-      </form>
+        <div className='col'>
+          <p className='text-center'>Login</p>
+          <form>
+            <div className='form-group'>
+              <label>E-mail</label>
+              <input
+                type='email'
+                className='form-control'
+                onChange={handleChange}
+              />
+
+              <label>Senha</label>
+              <input
+                type='password'
+                className='form-control'
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              // onClick={getData()}
+              className='btn btn-primary'
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
