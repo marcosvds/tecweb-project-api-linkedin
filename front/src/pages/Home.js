f2fd372aad58bc1d03d6b11a1f7cae32981b2750
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-11-07 11:33:36 -0300

    profile done

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,7 +4,7 @@ import api from '../api';
 export default function Home() {
   const [userData, setUserData] = useState([]);
 
-  const example = {
+  const dataExample = {
     user: {
       _id: '5fa54d7435423d85f08d3983',
       name: 'TCHIsdfghjkTrrCHhjkERO',
@@ -16,8 +16,6 @@ export default function Home() {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYTU0ZDc0MzU0MjNkODVmMDhkMzk4MyIsImlhdCI6MTYwNDY2ODc5OSwiZXhwIjoxNjA0NzU1MTk5fQ.4FqaRQFtMjJEwHb4PEQcBdJdY4l0eEz5JfTW_dZyxeg',
   };
 
-  console.log(JSON.stringify(example));
-
   // useEffect(() => {
   //   (async () => {
   //     const { data } = await api.get('/home');
@@ -25,5 +23,52 @@ export default function Home() {
   //   })();
   // }, []);
 
-  return <h1>Home</h1>;
+  const style = {
+    button: {
+      backgroundColor: '#c93b59',
+      letterSpacing: '0.07em',
+      textTransform: 'uppercase',
+      border: 'none',
+    },
+  };
+
+  return (
+    <div style={{ fontFamily: 'Lato' }} className='container p-3'>
+      <div className='d-flex flex-column'>
+        <div>
+          <p
+            style={{ letterSpacing: '0.1em', textTransform: 'uppercase' }}
+            className='h4 text-center'
+          >
+            LinkedIn API
+          </p>
+        </div>
+        <div>
+          <p className='h3'>Olá, user</p>
+        </div>
+        <div>
+          <label className='mr-4'>
+            Para acessar a foto, o nome, data de nascimento e tipo de perfil:
+          </label>
+          <button
+            style={style.button}
+            className='btn btn-primary m-2'
+            type='button'
+          >
+            Profile
+          </button>
+        </div>
+        <div>
+          <label className='mr-4'>Para acessar localização:</label>
+          <button
+            style={style.button}
+            className='btn btn-primary'
+            type='button'
+          >
+            Location
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- front/src/routes.js
+++ front/src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path='/login' component={Login} />
       <Route exact path='/home' component={Home} />
+      <Route exact path='/profile' component={Profile} />
     </Switch>
   );
 }
